332ce4870a924ff4b0f5bcc394a23584bc4b42c1
WillowSauceR <WillowSauceR@outlook.com>
2022-08-29 22:44:07 +0800

    support scan ip seguments

--- README.md
+++ README.md
@@ -6,13 +6,15 @@
 
 ## scan.py
 
-### 使用方法: `python3 scan.py [IP]`
+### 使用方法: `python3 scan.py [目标地址] [是否启用详细输出] [可选:超时时间，单位为秒，默认无限] [可选:保存结果的文件名]`
 
-#### 描述: 扫描一个IP上的所有BE协议服务器，使用前建议安装[Npcap](https://npcap.com/dist/npcap-1.60.exe)，然后使用命令 `pip install scapy`来安装依赖，基于scapy，基本不漏服
+#### 描述: 扫描IP上的所有BE协议服务器，使用前建议安装[Npcap](https://npcap.com/dist/npcap-1.60.exe)，然后使用命令 `pip install scapy`来安装依赖，基于scapy，基本不漏服
+
+#### 注意：目标地址可以填域名，IP或者IP段。如 mc.163.com，11.4.5.14，191.191.81-255.0-255
 
 ## send.py
 
-### 使用方法: `python3 send.py [IP] [端口] [载体包文件] [次数] [间隔:秒] [自动显示MOTD] [使用代理] [代理的国家，如cn, ru, us]`
+### 使用方法: `python3 send.py [目标地址] [端口] [载体包文件] [次数] [间隔:秒] [自动显示MOTD] [使用代理] [代理的国家，如cn, ru, us]`
 
 #### 描述: 发包复现工具, 需要一个内含byte数据的文件，一般为.dmp后缀名
 
@@ -20,7 +22,7 @@
 
 ## motd.py
 
-### 使用方法: `python3 motd.py [IP] [端口]`
+### 使用方法: `python3 motd.py [目标地址] [端口]`
 
 #### 描述: motd一个BE服务器，支持自动解析返回的数据
 
--- scan.py
+++ scan.py
@@ -1,10 +1,13 @@
+import socket
+import sys
+import threading
+import time
 from os import _exit
 from random import randint
-import socket
-import threading
-import sys
-import time
+
 from scapy.all import *
+from scapy.layers.inet import IP, UDP
+
 from api import getLocalHostIP
 
 localHostIP = getLocalHostIP()
@@ -12,60 +15,137 @@ localHostPort = randint(1024, 65535)
 
 try:
     TargetAddr = str(sys.argv[1])
+    verboseMode = str(sys.argv[2])
 except:
     TargetAddr = input("Target IP: ")
+    verboseMode = input("Show verbose info(y/n): ")
+
+try:
+    timeout = int(sys.argv[3])
+except:
+    timeout = None
+
+try:
+    fileName = str(sys.argv[4])
+except:
+    fileName = ""
+
 motdData = b'\x01\x00\x00\x00\x00$\r\x12\xd3\x00\xff\xff\x00\xfe\xfe\xfe\xfe\xfd\xfd\xfd\xfd\x124Vx\n'
 serverCount = 0
 
 
-def sendPacket(startNum, count):
+def getIpList(ip: str):
+    ipList = []
+    if os.path.exists(TargetAddr):
+        with open(TargetAddr, "r") as file:
+            for ip in file.readlines():
+                ipList.append(ip[:-1])
+            return ipList
+    try:
+        int(ip[-1])
+        # ip = "8.8.1-5.8-11"
+        processedIpSegs = [[], [], [], []]
+        ipSegIndex = 0
+        ipSegs = ip.split(".")  # 8.8.0-255.0-5 -> [8, 8, 0-255, 0-5]
+        for ipSeg in ipSegs:
+            if "-" in ipSeg:
+                segList = ipSeg.split("-")  # 0-5 -> [0, 5]
+                # range(0, 5)  -> [0, 1, 2, 3, 4]
+                for i in range(int(segList[0]), int(segList[1])+1):
+                    processedIpSegs[ipSegIndex].append(str(i))
+            else:
+                processedIpSegs[ipSegIndex].append(ipSeg)
+            ipSegIndex += 1
+
+        # [['8'], ['8'], ['1', '2', '3', '4'], ['8', '9', '10']]
+        for processedIpSegA in processedIpSegs[0]:
+            for processedIpSegB in processedIpSegs[1]:
+                for processedIpSegC in processedIpSegs[2]:
+                    for processedIpSegD in processedIpSegs[3]:
+                        ipList.append(
+                            f"{processedIpSegA}.{processedIpSegB}.{processedIpSegC}.{processedIpSegD}")
+        return ipList
+    except:
+        return [ip]
+
+
+def sendPacket(startNum, count, ip):
     port = startNum
     while True:
+        if stopThread:
+            break
         Time = time.strftime('%H:%M:%S')
-        if port % 1000 == 0:
+        if port % 1000 == 0 and verboseMode == "y":
             print(f"[{Time} I] Scaning port: {str(port)} ~ {str(port + 1000)}")
-        send(IP(src=localHostIP, dst=TargetAddr) / UDP(sport=localHostPort, dport=port) /
+        send(IP(src=localHostIP, dst=ip) / UDP(sport=localHostPort, dport=port) /
              motdData,
              verbose=False)
         if port == 65535:
-            print(f"[{Time} I] Port {startNum} ~ 65535 Done")
+            if verboseMode == "y":
+                print(f"[{Time} I] Port {startNum} ~ 65535 Done")
             while True:
+                if threading.enumerate().__len__() == 2:
+                    break
                 time.sleep(1)
         elif port == startNum + count - 1:
-            print(f"[{Time} I] Port {startNum} ~ {startNum + count} Done")
-            time.sleep(10)
-            print("BE Server Count: " + str(serverCount))
-            print("BDS Count: " + str(bdsCount))
-            print("NK Count: " + str(nkCount))
-            print("Geyser Count: " + str(geyserCount))
-            print("Skipped Count: " + str(skipped))
-            print("Error Count: " + str(error))
-            print("Total Player Count: " + str(totalPlayerCount))
-            _exit(0)
+            if verboseMode == "y":
+                print(f"[{Time} I] Port {startNum} ~ {startNum + count} Done")
+            break
         port += 1
 
 
-t1 = threading.Thread(target=sendPacket, args=(0, 10000))
-t2 = threading.Thread(target=sendPacket, args=(10000, 10000))
-t3 = threading.Thread(target=sendPacket, args=(20000, 10000))
-t4 = threading.Thread(target=sendPacket, args=(30000, 10000))
-t5 = threading.Thread(target=sendPacket, args=(40000, 10000))
-t6 = threading.Thread(target=sendPacket, args=(50000, 10000))
-t7 = threading.Thread(target=sendPacket, args=(60000, 5535))
-t1.setDaemon(True)
-t2.setDaemon(True)
-t3.setDaemon(True)
-t4.setDaemon(True)
-t5.setDaemon(True)
-t6.setDaemon(True)
-t7.setDaemon(True)
-t1.start()
-t2.start()
-t3.start()
-t4.start()
-t5.start()
-t6.start()
-t7.start()
+def startThreads():
+    global stopThread
+    ipList = getIpList(TargetAddr)
+    for ip in ipList:
+        time.sleep(1)
+        stopThread = False
+        print()
+        print(
+            f"[{time.strftime('%H:%M:%S')} I] Scaning target: {ip}")
+        print()
+        t1 = threading.Thread(target=sendPacket, args=(0, 10000, ip))
+        t2 = threading.Thread(target=sendPacket, args=(10000, 10000, ip))
+        t3 = threading.Thread(target=sendPacket, args=(20000, 10000, ip))
+        t4 = threading.Thread(target=sendPacket, args=(30000, 10000, ip))
+        t5 = threading.Thread(target=sendPacket, args=(40000, 10000, ip))
+        t6 = threading.Thread(target=sendPacket, args=(50000, 10000, ip))
+        t7 = threading.Thread(target=sendPacket, args=(60000, 5535, ip))
+        t1.setDaemon(True)
+        t2.setDaemon(True)
+        t3.setDaemon(True)
+        t4.setDaemon(True)
+        t5.setDaemon(True)
+        t6.setDaemon(True)
+        t7.setDaemon(True)
+        t1.start()
+        t2.start()
+        t3.start()
+        t4.start()
+        t5.start()
+        t6.start()
+        t7.start()
+        tmpServerCount = serverCount
+        if timeout:
+            time.sleep(timeout)
+        if tmpServerCount == serverCount:
+            stopThread = True
+        while threading.enumerate().__len__() != 2:  # main and itself
+            time.sleep(1)
+
+    print("BE Server Count: " + str(serverCount))
+    print("BDS Count: " + str(bdsCount))
+    print("NK Count: " + str(nkCount))
+    print("Geyser Count: " + str(geyserCount))
+    print("Skipped Count: " + str(skipped))
+    print("Error Count: " + str(error))
+    print("Total Player Count: " + str(totalPlayerCount))
+    _exit(0)
+
+
+t = threading.Thread(target=startThreads)
+t.setDaemon(True)
+t.start()
 
 bdsCount = 0
 nkCount = 0
@@ -127,6 +207,10 @@ while True:
         totalPlayerCount += int(infos[4])
         print(f"[{date} C] {str(serverCount)}")
         print(f"[{date} P] {totalPlayerCount}")
+        if fileName:
+            with open(fileName, "a+") as file:
+                file.write(
+                    f"{date} | {serverCount} | {addr[0]} | {addr[1]} | {infos[1]} | {infos[3]} | {infos[4]}\n")
         if len(infos) == 10 or len(infos) == 6:
             nkCount += 1
         elif re.search(b"edicated", data):
@@ -136,7 +220,9 @@ while True:
         elif re.search(b"eyser", data):
             geyserCount += 1
         sk_rec.close()
-    except:
-        print(f"[{time.strftime('%H:%M:%S')} R] An error occurred, skipped.")
+    except OSError:
+        pass
+    except Exception as info:
+        print(f"[{time.strftime('%H:%M:%S')} R] {info}, skipped.")
         error += 1
         pass
